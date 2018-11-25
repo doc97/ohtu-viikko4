2d442bf5cad51538869f5abd962387fe448378a2
doc97 <daniel.riissanen@gmail.com>
2018-11-25 18:12:42 +0200

    Verkkokauppa: Add 2 tests to KauppaTest.java

--- VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -114,4 +114,57 @@ public class KauppaTest {
 
         verify(pankki).tilisiirto(eq(nimi), eq(viitenumero), eq(tilinumero), anyString(), eq(hintasumma));
     }
+
+    @Test
+    public void aloitaAsiointiNollaaEdellistenOstoksenTiedot() {
+        String nimi = "pelle";
+        String tilinumero = "12345";
+        int viitenumero = 24;
+        int hintasumma = 5;
+
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(viitenumero);
+
+        Varasto varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu(nimi, tilinumero);
+
+        verify(pankki).tilisiirto(eq(nimi), eq(viitenumero), eq(tilinumero), anyString(), eq(hintasumma));
+    }
+
+    @Test
+    public void kauppaPyytaaUudenViitenumeronJokaiselleTapahtumalle() {
+        String nimi = "pelle";
+        String tilinumero = "12345";
+        int viitenumero = 24;
+
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(viitenumero);
+
+        Varasto varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu(nimi, tilinumero);
+
+        verify(viite, times(1)).uusi();
+    }
 }
