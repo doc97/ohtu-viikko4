4eeaf303f1e929e2bf92f7d64d96112164055680
doc97 <daniel.riissanen@gmail.com>
2018-11-25 18:40:11 +0200

    Verkkokauppa: Add more tests

--- VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -167,4 +167,65 @@ public class KauppaTest {
 
         verify(viite, times(1)).uusi();
     }
+
+    @Test
+    public void tuotteenPoistaminenKoristaKutsuuVarastonMetodeja() {
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1); // +1 haeTuote()
+        k.poistaKorista(1); // +1 haeTuote()
+
+        verify(varasto, times(2)).haeTuote(1);
+        verify(varasto, times(1)).palautaVarastoon(any());
+    }
+
+    @Test
+    public void poistetustaTuotteestaEiMakseta() {
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(5);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "kaakao", 10));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.poistaKorista(1);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), eq(10));
+    }
+
+    @Test
+    public void tuotteenPoistoAinoastaanKunTuoteOnKorissa() {
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        k.aloitaAsiointi();
+        k.poistaKorista(1);
+
+        verify(varasto, times(0)).palautaVarastoon(any());
+    }
 }
